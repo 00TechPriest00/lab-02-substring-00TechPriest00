66fc77eab07e5834484d5222cf7a4be56db33151
00TechPriest00 <47656939+00TechPriest00@users.noreply.github.com>
2019-05-12 21:19:15 +0300

    add files
    add files

---
+++ include/SearchAlgo.h
@@ -0,0 +1,28 @@
+#ifndef SearchAlgo_h
+#define SearchAlgo_h
+
+#include <stdio.h>
+#include "stdafx.h"
+#include <stdlib.h>
+#include <iostream>
+#include <string.h> 
+#include <math.h>
+#include <utility>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstddef>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int strLen(const string& str);
+int powX(int x, int y);
+size_t str_find(const string& str, const string& substr);
+size_t rk_find(const string& str, const string& substr);
+void test(string findStr, string str);
+
+#endif
