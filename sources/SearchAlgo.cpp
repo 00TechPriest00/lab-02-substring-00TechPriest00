705ada2abbfa37033153d82eca203bed1dffc4c2
00TechPriest00 <47656939+00TechPriest00@users.noreply.github.com>
2019-05-23 10:30:48 +0300

    Update SearchAlgo.cpp

--- sources/SearchAlgo.cpp
+++ sources/SearchAlgo.cpp
@@ -1,240 +1,180 @@
 #include "SearchAlgo.h"
+#define PRIMEVAL 2
+
 struct arch
 {
-    int buff = 0;
-    int warning = 0;
+	int buff = 0;
+	int warning = 0;
 };
 
 int strLen(const string& str)
 {
-    int count = 0;
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        count++;
-        i++;
-    }
-    return count;
+	int count = 0;
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return count;
 }
-int powX(int x, int y)
+long long powX(long long x, long long y)
 {
-    if (y == 0)
-        return 1;
+	if (y == 0)
+		return 1;
 
-    int retval = x;
-    while (--y > 0)
-    {
-        retval *= x;
-    }
-    return retval;
+	long long retval = x;
+	while (y > 0)
+	{
+		y--;
+		retval *= x;
+	}
+	return retval;
 }
 
 size_t str_find(const std::string& str, const std::string& substr)
 {
-    int a = strLen(str);
-    int b = strLen(substr);
-    for (int i = 0; i < b; i++)
-    {
-        if (substr[0] == str[i])
-        {
-            int j = 0;
-            for (; j < a; j++)
-            {
-                if (substr[j] != str[i + j])
-                {
-                    break;
-                }
-            }
-            if (j == a)
-            {
-                return i;
-            }
-        }
-    }
-    return -1;
+	int a = strLen(str);
+	int b = strLen(substr);
+	for (int i = 0; i < a; i++)
+	{
+		if (substr[0] == str[i])
+		{
+			int j = 0;
+			for (; j < b; j++)
+			{
+				if (substr[j] != str[i + j])
+				{
+					break;
+				}
+			}
+			if (j == b)
+			{
+				return i;
+			}
+		}
+	}
+	return -1;
 }
 
 size_t rk_find(const std::string& str, const std::string& substr)
 {
-    int prime = 11;
-    int oldPrime = 11;
-    int hush = 0;
-    char a;
-    for (int i = 0; i < strLen(substr); i++)
-    {
-        a = substr[i];
-        if (i == 0)
-        {
+	long long hush = 0;
+	long long buff = 0;
+	long long prime = PRIMEVAL;
+	int cntr1 = 0;
+	//int cntr2 = strLen(substr);
+	int totalch = strLen(str);
+	int len = strLen(substr);
 
-            hush = (int)a;
-        }
-        else
-        {
-        hush += ((int)a)*prime;
-        prime = prime*oldPrime;
-        }
-    }
-    prime = 11;
-    int len = strLen(substr);
-    int buff = 0;
-    for (int j = 0; j < len; j++)
-    {
-        a = str[j];
-        if (j == 0)
-        {
-            buff = (int)a;
-        }
-        else
-        {
-        buff += ((int)a)*prime;
-        prime = prime*oldPrime;
-        }
-    }
-    prime = 11;
-    int cntr1 = 0;
-    int cntr2 = strLen(substr);
-    int totalch = strLen(str);
-    while (buff != hush)
-    {
-        buff = (buff - ((int)str[cntr1])) / prime + ((int)str[cntr2])*powX(prime,len-1);
-        cntr1++;
-        cntr2++;
-        if (cntr2 > totalch)
-        {
-            return -1;
-        }
-    }
-    int cntr3 = cntr1;
-    for (int k = 0; k < strLen(substr); k++)
-    {
-        if (str[cntr1] == substr[k])
-        {
-            cntr1++;
-        }else
-        {
-            return -1;//Слово не найдено
-        }
-    }
-    return cntr3;
+	hush = calcHush(substr, strLen(substr), 0);
+	buff = calcHush(str, strLen(substr), 0);
+label:
+	while (buff != hush)
+	{
+		if (cntr1 + len >= totalch)
+		{
+			return -1;
+		}
+
+		long long a, b, c, d, e;
+		a = buff - (int)str[cntr1];
+		b = a / prime;
+		c = pow(prime, len - 1);
+		e = ((int)str[cntr1 + len])*c;
+		buff = b + e;
+		//buff = (buff - ((int)str[cntr1])) / prime + ((int)str[cntr2])*powX(prime, len - 1);
+		//if (buff != hush)
+		cntr1++;
+		//cntr2++;
+	}
+	int cntr3 = cntr1;
+	for (int k = 0; k < strLen(substr); k++)
+	{
+		if (str[cntr1] == substr[k])
+		{
+			cntr1++;
+		}
+		else
+		{
+			if (cntr1 < totalch - 1)
+			{
+				buff = calcHush(str, strLen(substr), cntr3 + 1);
+				cntr1 = cntr3 + 1;
+				goto label;
+			}
+			return -1;//Слово не найдено
+		}
+	}
+	return cntr3;
+}
+
+long long calcHush(const string & str, int len, int indStart)
+{
+	long long prime = PRIMEVAL;
+	int oldPrime = PRIMEVAL;
+	long long hush = 0;
+	char a;
+	//int leN = strLen(substr);
+	for (int i = indStart; i < indStart + len; i++)
+	{
+		a = str[i];
+		if (i == indStart)
+		{
+
+			hush = (int)a;
+		}
+		else
+		{
+			hush += ((int)a)*prime;
+			prime = prime*oldPrime;
+			if (hush >= 9223372036854775807)
+			{
+				hush = 0;
+			}
+		}
+	}
+	return hush;
 }
 
 //Алгоритм считающий длинну префикс функции
-int *prefFunc(std::string str)
+vector<int> prefFunc(string s) 
 {
-    int len = strLen(str);
-    int counter = 0;
-    int j = 0;
-    int saverI = 0;
-
-    int *prefFunc1 = new int[len];
-    memset(prefFunc1, 0, len*sizeof(int));
-
-    for (int i = 0; i < len; i++)
-    {
-        if (i == 0)
-        {
-            i++;
-        }
-        if (str[j] == str[i])
-        {
-            counter++;
-            prefFunc1[i] = counter;
-            if (i+1 <= len)
-            {
-            j++;
-            saverI = i;
-            i++;
-            while (str[j] == str[i])
-            {
-                counter++;
-                prefFunc1[i] = counter;
-                if (i+1 <= len)
-                {
-                j++;
-                i++;
-                }else
-                {
-                    goto flag;
-                }
-            }
-            if (str[j] != str[i])
-            {
-                counter = 0;
-            }
-            if (i < len)
-            {
-            i = saverI;
-            }
-            }
-        }
-            flag:
-            j = 0;
-            if (str[i] != str[0] && str[i] != str[j])
-            {
-                counter = 0;
-            }
-    }
-    return prefFunc1;
+	int n = s.length();
+	vector<int> intArr(n);
+	intArr[0] = 0;
+	for (int i = 1; i<n; ++i) 
+	{
+		int j = intArr[i - 1];
+		while (j > 0 && s[i] != s[j])
+		{
+			j = intArr[j - 1];
+		}
+		if (s[i] == s[j])  ++j;
+		{
+			intArr[i] = j;
+		}
+	}
+	return intArr;
 }
 
-/*int* prefFunc(string str)
-{
-    int len = strLen(str);
-    int counter = 0;
-    int j = len;
-    int saverI = 0;
-    int saverJ = 0;
-
-    //arch arh;
-
-    int *prefFunc1 = new int[len];
-
-    for (int i = 0; i < len-1; i++)
-    {
-        if (i>0)
-        {
-            j--;
-        }
-        while (str[i] == str[j])
-        {
-            saverI = i;
-            saverJ = j;
-            i++;
-            j--;
-            counter++;
-            prefFunc1[i] = counter;
-        }
-        counter = 0;
-        i = saverI;
-        j = saverJ;
-    }
-    return prefFunc1;
-}*/
-
 //Алгоритм Кнута - Морриса - Пратта
-size_t kmp_find(std::string findStr, string str)
+size_t kmp_find(string str, std::string findStr)
 {
-    int *prefFindStr = prefFunc(findStr);
+	vector<int> pIntArr = prefFunc(findStr);
+	int sLen = str.length();
+	for (int k = 0, i = 0; i < sLen; i++)
+	{
+		while ((k > 0) && (findStr[k] != str[i]))
+			k = pIntArr[k - 1];
 
-    int index = 0;
-    int i = 0;
-    int lenStr = strLen(str);
-    int iskZnach;
-    int lenFindStr = strLen(findStr);
+		if (findStr[k] == str[i])
+			k++;
 
-    while (i < lenStr)
-    {
-        while (findStr[index] == str[i] && index < lenFindStr)
-        {
-            i++;
-            index++;
-        }
-        if (index < lenFindStr)
-        {
-        index = prefFindStr[index - 1];
-        }
-    }
-    iskZnach = i - index + 1;
-    delete[] prefFindStr;
-    return iskZnach;
+		if (k == findStr.length())
+			return (i - findStr.length() + 1); //либо продолжаем поиск следующих вхождений
+	}
+
+	return -1;
+
 }
